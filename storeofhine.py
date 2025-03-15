8de0229e2388d9bc51f91d7c8efafc8b9a982d61
DuongThiThuHine <thuhien2004nt@gmail.com>
2025-03-15 15:55:21 +0700

    Update storeofhine.py

--- storeofhine.py
+++ storeofhine.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Load the product data
-df_products = pd.read_csv('D:/STREAMLIT/san_pham_tien_loi_100.csv')
+df_products = pd.read_csv('san_pham_tien_loi_100.csv')
 
 # Initialize session state for cart
 if 'cart' not in st.session_state:
